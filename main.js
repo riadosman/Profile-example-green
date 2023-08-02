d0eee3dc398334d75a6a89b238a5b4e31f731d54
riad osman <riyados973@gmail.com>
2023-08-02 08:18:02 +0300

    add the new skills

--- main.js
+++ main.js
@@ -1,43 +1,47 @@
 let projects = document.querySelector(".projects");
 // skills click
 let skills = document.querySelectorAll(".skill");
-let srcs = ["https://en.wikipedia.org/wiki/Bootstrap_(front-end_framework)","https://en.wikipedia.org/wiki/CSS","https://en.wikipedia.org/wiki/Figma_(software)",
-"https://en.wikipedia.org/wiki/Git","https://en.wikipedia.org/wiki/HTML","https://en.wikipedia.org/wiki/JavaScript"]
-skills.forEach((e,index)=>{
- e.addEventListener("click",()=>{
-  window.open(srcs[index],"_blank")
- })
+let srcs = ["https://en.wikipedia.org/wiki/Bootstrap_(front-end_framework)", "https://en.wikipedia.org/wiki/CSS", "https://en.wikipedia.org/wiki/Figma_(software)",
+  "https://en.wikipedia.org/wiki/Git", 
+  "https://en.wikipedia.org/wiki/HTML",
+   "https://en.wikipedia.org/wiki/JavaScript",
+   "https://en.wikipedia.org/wiki/JQuery",
+  "https://en.wikipedia.org/wiki/React_(software)"]
+skills.forEach((e, index) => {
+  e.addEventListener("click", () => {
+    window.open(srcs[index], "_blank")
+  })
 })
 // cv click
 let cv = document.querySelector(".cv");
-cv.addEventListener("click",()=>{
-  window.open("https://drive.google.com/file/d/1AS0MeYz-_15lxKXf5_rmNEu6a4oyBeLG/view?usp=sharing","_blank")
+cv.addEventListener("click", () => {
+  window.open("https://drive.google.com/file/d/1AS0MeYz-_15lxKXf5_rmNEu6a4oyBeLG/view?usp=sharing", "_blank")
 })
 // get data from json file
 fetch("project.json").then(
-  (res)=>{
+  (res) => {
     return n = res.json();
   }
-).then((n)=>{
+).then((n) => {
   let keys = Object.keys(n);
   for (let i = 0; i < 3; i++) {
-    createProject(n[keys[i]].img,n[keys[i]].name,n[keys[i]].time,n[keys[i]].description,n[keys[i]].link)
+    createProject(n[keys[i]].img, n[keys[i]].name, n[keys[i]].time, n[keys[i]].description, n[keys[i]].link)
   }
 
   // make the pagination system
   let pags = document.querySelectorAll(".pagination span");
-  pags.forEach((e)=>{
-    e.addEventListener("click",()=>{
+  pags.forEach((e) => {
+    e.addEventListener("click", () => {
       projects.innerHTML = "";
-      let start = [1,4,7,10,13,16]
+      let start = [1, 4, 7, 10, 13, 16]
       for (let i = start[+e.innerHTML - 1]; i < start[+e.innerHTML - 1] + 3; i++) {
-        createProject(n[`project-${i}`].img,n[`project-${i}`].name,n[`project-${i}`].time,n[`project-${i}`].description,n[`project-${i}`].link)
+        createProject(n[`project-${i}`].img, n[`project-${i}`].name, n[`project-${i}`].time, n[`project-${i}`].description, n[`project-${i}`].link)
       }
     })
   })
 })
 // create projects function
-function createProject(i,n,t,d,l){
+function createProject(i, n, t, d, l) {
   let div = document.createElement("div");
   div.classList.add("project");
   div.style.backgroundImage = `url(${i})`;
@@ -51,7 +55,7 @@ function createProject(i,n,t,d,l){
   let name = document.createElement("h3");
   let title = document.createTextNode(n); // title
   hoverText.appendChild(name);
-  name.appendChild(title); 
+  name.appendChild(title);
   let time = document.createElement("p");
   let timeText = document.createTextNode(t); // time
   hoverText.appendChild(time);
@@ -64,42 +68,42 @@ function createProject(i,n,t,d,l){
   button.classList.add("go")
   let btntext = document.createTextNode("Go Site");
   button.appendChild(btntext);
-  button.onclick = function(){
-    window.open(l,"_blank")
+  button.onclick = function () {
+    window.open(l, "_blank")
   }
   hoverText.appendChild(button)
 }
 
 let cert = document.querySelectorAll(".certs img");
-let certsrcs = ["https://drive.google.com/file/d/1ruq08uArmQDcYWFVSfAVHTPY7m2vM1ZV/view?usp=sharing","https://drive.google.com/file/d/1EDTkgwa-mZvnR_9JAqCvd3-WFzOWNv9_/view?usp=sharing","https://www.m3aarf.com/storage/cer/7858445374.jpg"]
-cert.forEach((e,index)=>{
- e.addEventListener("click",()=>{
-  window.open(certsrcs[index],"_blank")
- })
+let certsrcs = ["https://drive.google.com/file/d/1ruq08uArmQDcYWFVSfAVHTPY7m2vM1ZV/view?usp=sharing", "https://drive.google.com/file/d/1EDTkgwa-mZvnR_9JAqCvd3-WFzOWNv9_/view?usp=sharing", "https://www.m3aarf.com/storage/cer/7858445374.jpg"]
+cert.forEach((e, index) => {
+  e.addEventListener("click", () => {
+    window.open(certsrcs[index], "_blank")
+  })
 })
 // Add an event listener to the 'load' event
-window.addEventListener('load', function() {
+window.addEventListener('load', function () {
   // Scroll to the top of the page
   window.scrollTo(0, 0);
 });
-if(window.innerWidth < 1000 ){
-  swal("For a better experience", "please use a laptop or computer")  
+if (window.innerWidth < 1000) {
+  swal("For a better experience", "please use a laptop or computer")
 }
 
 // communicate
 let links = document.querySelectorAll(".links .link")
-let linkSrcs = ["https://www.facebook.com/riyad.osman.35/","https://github.com/riadosman/","mailto:riyados973@gmail.com","https://www.linkedin.com/in/riad-osman-6598ba24a/","https://www.upwork.com/freelancers/~01b653a11cced80aff"]
-links.forEach((e,i)=>{
-  e.addEventListener("click",()=>{
-    window.open(linkSrcs[i],"_blank")
+let linkSrcs = ["https://www.facebook.com/riyad.osman.35/", "https://github.com/riadosman/", "mailto:riyados973@gmail.com", "https://www.linkedin.com/in/riad-osman-6598ba24a/", "https://www.upwork.com/freelancers/~01b653a11cced80aff"]
+links.forEach((e, i) => {
+  e.addEventListener("click", () => {
+    window.open(linkSrcs[i], "_blank")
   })
 })
 
 // loading
-window.onload = function(){
-    let spn = document.querySelector(".spinner")
-    spn.style.display = "flex";
-    setTimeout(()=>{
-        spn.style.display = "none";
-    },2000)
+window.onload = function () {
+  let spn = document.querySelector(".spinner")
+  spn.style.display = "flex";
+  setTimeout(() => {
+    spn.style.display = "none";
+  }, 2000)
 }
